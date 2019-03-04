713b04a244f3dcce6073925537166be45160ea0b
Dell <jaehyun0803@naver.com>
2019-03-04 20:18:01 +0900

    gitignore commit

--- morpheme/models.py
+++ morpheme/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from konlpy.tag import *
 
 
 class Sentence(models.Model):
@@ -16,3 +17,20 @@ class CsvDocument(models.Model):
 class Morpheme(models.Model):
     sentence = models.CharField(max_length=300)
     title = models.CharField(max_length=300)
+
+    @staticmethod
+    def morpheme_init(morpheme_type):
+        morpheme_object = None
+        if morpheme_type == 'mecab':
+            morpheme_object = Okt()
+        elif morpheme_type == 'okt':
+            morpheme_object = Okt()
+        elif morpheme_type == 'komoran':
+            morpheme_object = Komoran()
+        elif morpheme_type == 'hannanum':
+            morpheme_object = Hannanum()
+        elif morpheme_type == 'kkma':
+            morpheme_object = Kkma()
+        elif morpheme_type == 'jiana':
+            morpheme_object = Okt()
+        return morpheme_object
